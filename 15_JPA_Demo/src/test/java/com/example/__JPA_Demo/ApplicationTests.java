73a0cb6d1d2e6b61b774fd6924217be790d58e15
Aman Maurya <114045665+AmanCoder01@users.noreply.github.com>
2025-08-19 20:13:27 +0530

    unamed

---
+++ 15_JPA_Demo/src/test/java/com/example/__JPA_Demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.__JPA_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
